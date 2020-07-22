4ae962e389328a44d4847f6115ad7a7d915483cc
Thever <574770209@qq.com>
2020-07-22 16:33:32 +0800

    添加demo文件
    添加demo文件

---
+++ pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="content">
+		<view class="uni-list">
+			<!-- :data-newsid 用于地址动态传参数 newsid -->
+			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item, index) in news" :key="index" @tap="openInfo" :data-newsid="item.post_id">
+				<view class="uni-media-list">
+					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+					<view class="uni-media-list-body">
+						<view class="uni-media-list-text-top">{{item.title}}</view>
+						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				news: []
+			}
+		},
+		onLoad() {
+			uni.showLoading({
+				title:'加载中...'
+			})
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news',
+				method: 'GET',
+				data: {},
+				success: res => {
+					this.news = res.data
+					uni.hideLoading()
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		},
+		methods: {
+			openInfo (e) {
+				//	通过自定义data传入来获取参数，当然你也可以直接传入必要内容
+				const newsid = e.currentTarget.dataset.newsid
+				console.error(newsid)
+				uni.navigateTo({
+					//	通过地址栏动态传参
+					url: '../info/info?newsid='+newsid,
+					success: res => {},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.uni-media-list-body{height: auto;}
+	.uni-media-list-text-top{line-height:1.6em;}
+</style>
---
+++ pages/info/info.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="content">
+		<view class="title">{{title}}...</view>
+		<view class="art-content">
+			<!-- nodes支持array和string -->
+			<rich-text class="richText" :nodes="strings"></rich-text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title:'',
+				strings: ''
+			}
+		},
+		//	页面载入成功获取传入的参数
+		onLoad(e) {
+			console.log(e)
+			uni.showLoading({
+				title:'加载中...'
+			})
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news/36kr/'+e.newsid,
+				method: 'GET',
+				data: {},
+				success: res => {
+					console.log(res)
+					this.title = res.data.title
+					this.strings = res.data.content
+					uni.hideLoading()
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.content{
+		padding:10rpx 2%;
+		width:96%;
+		flex-wrap: wrap;
+	}
+	.title{
+		line-height:2em;
+		font-weight:700;
+		font-size:38rpx;
+	}
+	.art-content{
+		line-height:2rem;
+	}
+</style>
